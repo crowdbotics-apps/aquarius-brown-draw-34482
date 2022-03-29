c3f330f40f77e7c2d20d03559bdf0f8f005ec83a
Crowdbotics <team@crowdbotics.com>
2022-03-29 03:15:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "aquarius_brown_draw_34482",
+  url: "https://aquarius_brown_draw_34482.botics.co",
+  api: "https://aquarius_brown_draw_34482.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
